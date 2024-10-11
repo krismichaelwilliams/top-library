b0a29aa9281c014faa8516eb32c7e7c7284f6e69
Kris Williams <99233291+krismichaelwilliams@users.noreply.github.com>
2024-10-10 21:15:36 -0500

    feat: adding card template for books being displayed on page

--- src/app/components/browse/browse.component.ts
+++ src/app/components/browse/browse.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Book } from '../../models/book.model';
+import { BookComponent } from '../book/book.component';
 
 @Component({
   selector: 'app-browse',
   standalone: true,
-  imports: [],
+  imports: [BookComponent],
   templateUrl: './browse.component.html',
   styleUrl: './browse.component.scss',
 })
--- src/app/components/view/view.component.ts
+++ src/app/components/view/view.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { AddBookComponent } from '../add-book/add-book.component';
 import { BrowseComponent } from '../browse/browse.component';
 
 @Component({
   selector: 'app-view',
   standalone: true,
-  imports: [BrowseComponent, AddBookComponent],
+  imports: [BrowseComponent],
   templateUrl: './view.component.html',
   styleUrl: './view.component.scss',
 })
